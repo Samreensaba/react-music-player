b41ce2e0c08195c8e063bc9a8f6883b8db8695b4
Samreensaba <Samren_saba@yahoo.com>
2022-07-08 11:23:58 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Player from "./components/Player";
+import Sidebar from "./components/Sidebar";
+import Home from "./components/Home";
+import { Row } from "react-bootstrap";
+import Artist from "./components/Artist";
+import Album from "./components/Album";
+import { useState } from "react";
+
+let headers = new Headers({
+  "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+  "X-RapidAPI-Key": "222902beabmshb95a65b737cead6p1f3ac9jsn23ced94c0d20",
+});
 
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
+
+  const search = async (string) => {
+    if (string.length > 2) {
+      try {
+        let response = await fetch(
+          "https://striveschool-api.herokuapp.com/api/deezer/search?q=" +
+            string,
+          {
+            method: "GET",
+            headers,
+          }
+        );
+
+        let result = await response.json();
+        let songs = result.data;
+
+        setSearchResults(songs);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container-fluid">
+        <Row>
+          <Sidebar search={search} />
+          <Routes>
+            <Route
+              path="/"
+              exact
+              element={<Home searchResults={searchResults} />}
+            />
+            <Route path="/artist/:id" element={<Artist />} />
+            <Route path="/album/:id" element={<Album />} />
+          </Routes>
+        </Row>
+      </div>
+      <Player />
+    </Router>
   );
 }
 
